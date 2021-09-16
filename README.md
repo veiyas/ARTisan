f2324fcedaa33dbd315c67946c4bbf6d893cc49c
veiyas <david.rk97@gmail.com>
2021-09-16 11:41:55 +0200

    Changed script to assume the GPU should be used if you haven't defined a config.json. Updatd readme to reflect this

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # ARTisan
 Uses deep learning to create art.
 
-You must define a local config.json to determine whether to use your CPU or GPU. If you have an AMD card and are running Windows, you need to use the CPU. In config.json:
-
+If you have an AMD card and are running Windows, you need to use the CPU. Define the file config.json:
 ```
 {
     "useGPU" : "True"
 }
 ```
+to make sure tensorflow uses the CPU.
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import json
 import os
-if not os.path.exists('config.json'):
-    print('config.json not found, stopping...')
-    quit()
-useGPU = json.loads(open('config.json',).read())['useGPU']
-
 import tensorflow
-if not eval(useGPU):
-    print('Not using GPU.')
-    tensorflow.config.set_visible_devices([], 'GPU')
+
+useGPU = True
+if os.path.exists('config.json'):
+    useGPU = json.loads(open('config.json',).read())['useGPU']
+    if not eval(useGPU):
+        print('Not using GPU')
+        tensorflow.config.set_visible_devices([], 'GPU')
 print('Tensorflow version:', tensorflow.__version__)
 
 from tensorflow import keras
